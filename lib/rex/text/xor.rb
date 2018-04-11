858ca860a7a394b90e02b9cec7a3eee018977165
Wei Chen <wei_chen@rapid7.com>
2018-04-10 19:10:10 -0500

    Aware of data type for xor key

--- lib/rex/text/xor.rb
+++ lib/rex/text/xor.rb
@@ -4,7 +4,7 @@ module Rex
   module Text
 
     def self.xor(key, value)
-      xor_key = key.to_i(16)
+      xor_key = key.kind_of?(Integer) ? key : key.to_i(16)
       unless xor_key.between?(0, 255)
         raise ArgumentError, 'XOR key should be between 0x00 to 0x0f'
       end
@@ -12,7 +12,7 @@ module Rex
       buf = ''
 
       value.each_byte do |byte|
-        xor_byte = byte ^ key
+        xor_byte = byte ^ xor_key
         xor_byte = [xor_byte].pack('c').first
         buf << xor_byte
       end
