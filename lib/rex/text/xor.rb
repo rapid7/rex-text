71bb07c1befc017b2b610c55c52e129c0d02137c
William Vu <William_Vu@rapid7.com>
2019-07-18 12:55:42 -0500

    Implement XOR with variable-length key

--- lib/rex/text/xor.rb
+++ lib/rex/text/xor.rb
@@ -1,28 +1,39 @@
 # -*- coding: binary -*-
 
-module Rex
-  module Text
-
-    # Returns a XOR'd string.
-    #
-    # @param key [String] XOR key.
-    # @param value [String] The string to XOR.
-    # @return [String] An XOR'd string.
-    def self.xor(key, value)
-      xor_key = key.kind_of?(Integer) || key.nil? ? key.to_i : key.to_i(16)
-      unless xor_key.between?(0, 255)
-        raise ArgumentError, 'XOR key should be between 0x00 to 0x0f'
-      end
-
-      buf = ''
-
-      value.each_byte do |byte|
-        xor_byte = byte ^ xor_key
-        buf << [xor_byte].pack('c')
-      end
-
-      buf
+module Rex::Text
+  # XOR a string against a variable-length key
+  #
+  # @param key [String] XOR key
+  # @param value [String] String to XOR
+  # @return [String] XOR'd string
+  def self.xor(key, value)
+    # Check for nil
+    unless key && value
+      raise ArgumentError, 'XOR key and value must be supplied'
     end
 
+    xor_key =
+      begin
+        # Support integer strings
+        Integer(key)
+      rescue ArgumentError
+        key
+      end
+
+    if xor_key.is_a?(Integer)
+      unless xor_key.between?(0, 255)
+        raise ArgumentError, 'XOR key must be between 0x00 and 0xff'
+      end
+
+      # Convert integer to string
+      xor_key = [xor_key].pack('C')
+    end
+
+    # Get byte arrays for key and value
+    xor_key   = xor_key.bytes
+    xor_value = value.bytes
+
+    # XOR value against cycled key
+    xor_value.zip(xor_key.cycle).map { |v, k| v ^ k }.pack('C*')
   end
 end
