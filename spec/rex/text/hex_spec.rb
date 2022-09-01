e27bc67bf7dcee23091bf1edb9ee5b38f19efc40
Spencer McIntyre <zeroSteiner@gmail.com>
2022-09-01 11:22:44 -0400

    Better specs

--- spec/rex/text/hex_spec.rb
+++ spec/rex/text/hex_spec.rb
@@ -2,22 +2,20 @@
 require 'spec_helper'
 
 RSpec.describe Rex::Text do
-  context "Class Hex methods" do
-    context ".hexify" do
-      let(:wrap) { 60 }
-      it 'should wrap at the specified columns' do
-        lines = described_class.hexify("A" * 100, wrap).split("\n")
-        expect(lines.any? { |line| line.rstrip.length > wrap }).to be_falsey
-      end
+  describe ".hexify" do
+    let(:wrap) { 60 }
+    it 'should wrap at the specified columns' do
+      lines = described_class.hexify("A" * 100, wrap).split("\n")
+      expect(lines.any? { |line| line.rstrip.length > wrap }).to be_falsey
+    end
 
-      it 'should wrap at the specified columns when a line start and end are specified' do
-        lines = described_class.hexify("A" * 100, wrap, '"', '"').split("\n")
-        expect(lines.any? { |line| line.rstrip.length > wrap }).to be_falsey
-      end
+    it 'should wrap at the specified columns when a line start and end are specified' do
+      lines = described_class.hexify("A" * 100, wrap, '"', '"').split("\n")
+      expect(lines).to all(have_maximum_width(wrap))
+    end
 
-      it 'should convert the buffer to hex' do
-        expect(described_class.hexify(Random.bytes(8))).to match(/(\\x[a-fA-F0-9]{2}){8}/)
-      end
+    it 'should convert the buffer to hex' do
+      expect(described_class.hexify(Random.bytes(8))).to match(/(\\x[a-fA-F0-9]{2}){8}/)
     end
   end
 end
--- spec/rex/text/lang_spec.rb
+++ spec/rex/text/lang_spec.rb
@@ -2,18 +2,16 @@
 require 'spec_helper'
 
 RSpec.describe Rex::Text do
-  context "Class Language methods" do
 
-    LANGUAGES = %w[ bash c csharp golang perl python ruby ]
+  LANGUAGES = %w[ bash c csharp golang perl python ruby ]
 
-    context "to language methods should wrap at the specified columns" do
-      LANGUAGES.each do |lang|
-        context ".to_#{lang}" do
-          it "should raise on non-convertable characters" do
-            wrap = 60
-            lines = described_class.send("to_#{lang}", "A" * 100, wrap).split("\n")
-            expect(lines.any? { |line| line.rstrip.length > wrap }).to be_falsey
-          end
+  context "to language methods should wrap at the specified columns" do
+    LANGUAGES.each do |lang|
+      describe ".to_#{lang}" do
+        it "should raise on non-convertable characters" do
+          wrap = 60
+          lines = described_class.send("to_#{lang}", "A" * 100, wrap).split("\n")
+          expect(lines).to all(have_maximum_width(wrap))
         end
       end
     end
