b4b08e58b54ff8a0a96d3f6c88832376b3bec3d7
Spencer McIntyre <zeroSteiner@gmail.com>
2026-05-13 14:07:58 -0400

    Remove to_hex_cstring method
    The functionality has moved to Metasm::Shellcode.define_data and
    Metasm::Shellcode.define_cstring, which emit full `db` directives and
    are a better home for assembly-source generation than rex-text.
    Reverts the addition from #76.

--- spec/rex/text/hex_spec.rb
+++ spec/rex/text/hex_spec.rb
@@ -3,34 +3,6 @@ require 'spec_helper'
 require 'timeout'
 
 RSpec.describe Rex::Text do
-  describe ".to_hex_cstring" do
-    it 'should return just a null terminator for an empty string' do
-      expect(described_class.to_hex_cstring("")).to eq("0x00")
-    end
-
-    it 'should return hex bytes with a null terminator for a non-empty string' do
-      expect(described_class.to_hex_cstring("AB")).to eq("0x41, 0x42, 0x00")
-    end
-
-    it 'should handle nil by treating it as an empty string' do
-      expect(described_class.to_hex_cstring(nil)).to eq("0x00")
-    end
-
-    it 'should handle binary data' do
-      expect(described_class.to_hex_cstring("\xff\x00\x01")).to eq("0xff, 0x00, 0x01, 0x00")
-    end
-
-    context 'with nullbyte: false' do
-      it 'should return an empty string for an empty input' do
-        expect(described_class.to_hex_cstring("", nullbyte: false)).to eq("")
-      end
-
-      it 'should return hex bytes without a null terminator' do
-        expect(described_class.to_hex_cstring("AB", nullbyte: false)).to eq("0x41, 0x42")
-      end
-    end
-  end
-
   describe ".hexify" do
     let(:wrap) { 60 }
     it 'should wrap at the specified columns' do
